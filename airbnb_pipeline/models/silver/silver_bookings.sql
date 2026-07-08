f99a7743f1880e5eaffa80f608c1337466dc221a
Omega-84 <nayyar.varun84@gmail.com>
2026-07-07 22:23:34 -0400

    Added silver layers with custom macros

---
+++ airbnb_pipeline/models/silver/silver_bookings.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='incremental',
+    unique_key='BOOKING_ID')}}
+
+SELECT 
+    booking_id,
+    listing_id,
+    booking_date,
+    {{ multiply('nights_booked','booking_amount',2)}} + cleaning_fee + service_fee AS total_amount,
+    booking_status,
+    created_at
+FROM
+    {{ ref("bronze_bookings")}}
